b80fe38a844efdae8acdaa1d3d580977aaccc4cd
Repository42 <EarthCallingProd@proton.me>
2026-03-31 21:08:16 +0100

    Create tstalk_schema.sql

---
+++ tstalk_schema.sql
@@ -0,0 +1,37 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+CREATE TABLE `activity` (
+  `uid` bigint(11) NOT NULL,
+  `timestamp` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `updates` (
+  `uid` bigint(11) NOT NULL,
+  `phone` text DEFAULT NULL,
+  `username` text DEFAULT NULL,
+  `first` text NOT NULL,
+  `last` text DEFAULT NULL,
+  `bio` text DEFAULT NULL,
+  `pfpcount` int(11) NOT NULL,
+  `currentpfpid` bigint(11) DEFAULT NULL,
+  `sharingtime` tinyint(1) NOT NULL,
+  `timestamp` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `users` (
+  `uid` bigint(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`uid`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
