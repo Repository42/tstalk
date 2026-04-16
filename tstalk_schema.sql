cf4ae8ddc6ac0bc11c5d32c83ca85a8670a43863
Repository42 <EarthCallingProd@proton.me>
2026-04-16 16:15:17 +0100

    Update tstalk_schema.sql

--- tstalk_schema.sql
+++ tstalk_schema.sql
@@ -7,7 +7,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
--- DROP DATABASE tstalk ;
+-- DROP DATABASE tstalk;
 CREATE DATABASE tstalk;
 USE tstalk;
 
